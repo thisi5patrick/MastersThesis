998427e2ac72040e715216c6f09aadfa9889d36e
thisi5patrick <bielen.patryk@student.pk.edu.pl>
2021-05-06 22:57:07 +0200

    Main window initialized. New files and directories added

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from src import Window
+
+if __name__ == '__main__':
+    root = Tk()
+
+    Window()
+
+    root.mainloop()
---
+++ src/window.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter.ttk import *
+
+
+class Window(Frame):
+    def __init__(self):
+        super().__init__()
+        self.master.title('Praca Magisterska')
+        self.initUI()
+
+    def initUI(self):
+        ...
